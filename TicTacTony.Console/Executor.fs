15e445d8d66af630928faa31c5879076648c882f
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2020-02-18 05:02:42 +0530

    Simplify API IGame is mandatory and no longer optional in internal APIs

--- TicTacTony.Console/Executor.fs
+++ TicTacTony.Console/Executor.fs
@@ -7,10 +7,10 @@ open Reader
 
 module Executor =
   
-  let private options f c u p g =
+  let private options g f c u p =
     [
       defaultValue [] (map (fun (p: IPlayable) -> p.Moves |> List.map (Move.position >> Move >> Some)) p)
-      defaultValue [] (map (fun (g: IGame) -> g.Positions |> List.map (PlayerAt >> Some)) g)
+      g.Positions |> List.map (PlayerAt >> Some)
       [ map (fun (f: IFilled) -> IsDraw) f ]
       [ map (fun (c: ICompleted) -> WhoWon) c ]
       [ map (fun (u: IUndoable) -> TakeBack) u ]
@@ -27,16 +27,16 @@ module Executor =
     | true -> "It's a draw."
     | false -> "It's not a draw."
 
-  let rec private step f c u p g game =
+  let rec private step game g f c u p =
     let print = printfn "%s"
     let _ = print "\n"
-    let _ = iter (fun g -> (Board.toString g.Board) |> print) g
+    let _ = Board.toString g.Board |> print
     let _ = iter (fun f -> (f.IsDraw () |> drawn) |> print) f
     let _ = iter (fun c -> (c.WhoWon () |> winner) |> print) c
-    let command = read (options f c u p g)
-    in handle f c u p g game command
+    let command = read (options g f c u p)
+    in handle game g f c u p command
 
-  and private handle f c u p g game command =
+  and private handle game g f c u p command =
       let impossible () = printfn "impossible!" |> exit 1
       let iff f x = match x with | Some x -> f x | None -> impossible ()
       let _ = printfn "%s" (Commands.toDescription command)
@@ -44,7 +44,7 @@ module Executor =
         match command with
         | New -> play Game.NewGame
         | Move p' -> iff (fun p -> p.Moves |> List.find (Move.position >> ((=) p')) |> p.Move |> play) p
-        | PlayerAt position -> iff (fun g -> let _ = printfn "%O" (g.PlayerAt position) in play game) g
+        | PlayerAt position -> let _ = printfn "%O" (g.PlayerAt position) in play game
         | IsDraw -> iff (fun f -> let _ = printfn "%O" (f.IsDraw ()) in play game) f
         | WhoWon -> iff (fun c -> let _ = printfn "%O" (c.WhoWon ()) in play game) c
         | TakeBack -> iff (fun u -> play (u.TakeBack ())) u
@@ -52,7 +52,7 @@ module Executor =
 
   and play game =
     match game with
-    | Begun (g, p) -> step None None None (Some p) (Some g) game
-    | InProgress (g, u, p) -> step None None (Some u) (Some p) (Some g) game
-    | Won (g, f', c, u) -> step f' (Some c) (Some u) None (Some g) game
-    | Drawn (g, f, c, u) -> step (Some f) (Some c) (Some u) None (Some g) game
+    | Begun (g, p) -> step game g None None None (Some p)
+    | InProgress (g, u, p) -> step game g None None (Some u) (Some p)
+    | Won (g, f', c, u) -> step game g f' (Some c) (Some u) None
+    | Drawn (g, f, c, u) -> step game g (Some f) (Some c) (Some u) None

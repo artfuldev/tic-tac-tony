94a3858a8bc083b4accf8dac51e813bcb2fdf6e6
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2020-02-18 18:02:15 +0530

    Use custom matchers for tests

---
+++ TicTacTony.Tests/Matchers.fs
@@ -0,0 +1,36 @@
+﻿namespace TicTacTony.Tests
+
+open TicTacTony.Core
+open NHamcrest.Core
+
+
+module Matchers =
+    
+    let private board game =
+        match game with
+        | Fresh (g, _) | Played (g, _, _)  | Won (g, _, _, _)
+        | Drawn (g, _, _, _) -> g.Board
+
+    let beGame game =
+        CustomMatcher (
+            sprintf "be game %O" game,
+            fun (game': obj) ->
+                match game' with
+                | :? Game as game'-> board game' = board game
+                | _ -> false
+        )
+
+    let bePlayer player =
+        CustomMatcher (
+            sprintf "be player %s" player,
+            fun (player': obj) ->
+                match player' with
+                | :? Option<Player> as player' ->
+                    player'
+                    |> Option.map Player.toString
+                    |> Option.defaultValue "_"
+                    |> ((=) player)
+                | _ -> false
+        )
+    
+
--- TicTacTony.Tests/Tests.fs
+++ TicTacTony.Tests/Tests.fs
@@ -1,52 +1,27 @@
 ﻿namespace TicTacTony.Tests
 
 open FsUnit
-open FsCheck
 open Xunit
 
 open TicTacTony.Core
+open Matchers
 
 module Tests =
 
-    let fail _ =
+    let private fail _ =
         false |> should equal true
 
-    let board game =
-        match game with
-        | Fresh (g, _) | Played (g, _, _)  | Won (g, _, _, _)
-        | Drawn (g, _, _, _) -> g.Board
-
-    let assertEquals expected actual =
-        board actual |> should equal (board expected)
-
-    let move game x =
+    let private move game x =
         match game with
         | Fresh (_, p) | Played (_, _, p) ->
-           p.Moves
-           |> Seq.find (Move.position >> ((=) x))
-           |> p.Move
+           p.Moves |> Seq.find (Move.position >> ((=) x)) |> p.Move
         | _ -> failwith "not found"
 
-    let play game positions =
+    let private play game positions =
         positions
         |> Seq.map Positions.parse
         |> Seq.choose id
         |> Seq.fold move game
-
-    [<Fact>]
-    let ``New game is a fresh game where the board is empty`` () =
-        match Game.NewGame with
-        | Fresh (g, _) ->
-            g.Positions
-            |> Seq.map g.PlayerAt
-            |> Seq.iter (should equal None)
-        | _ -> fail ()
-
-    [<Fact>]
-    let ``New game is a fresh game where the player is X`` () =
-        match Game.NewGame with
-        | Fresh (_, p) -> p.Player |> Player.toString |> should equal "X"
-        | _ -> fail ()
 
     [<Theory>]
     [<InlineData ("NW N", "NE", "NW N NE")>]
@@ -54,8 +29,8 @@ module Tests =
     let ``In a playable game, a valid move can be made to get a resulting game`` (moves: string)  (next: string) (result: string) =
         let current = moves.Split " " |> play Game.NewGame
         let expected = result.Split " " |> play Game.NewGame
-        let result = move current (Option.get (Positions.parse next))
-        in assertEquals expected result
+        let result = move current (next |> Positions.parse |> Option.get)
+        in result |> should beGame expected
         
     [<Theory>]
     [<InlineData ("NW N NE", "NW N")>]
@@ -65,7 +40,7 @@ module Tests =
         in
             match current with
             | Played (_, u, _) | Won (_, _, _, u) | Drawn (_, _, _, u) ->
-                assertEquals expected (u.TakeBack ())
+                u.TakeBack () |> should beGame expected
             | _ -> fail ()
 
     [<Theory>]
@@ -75,10 +50,7 @@ module Tests =
     let ``In a completed game, the winner can be queried`` (moves: string)  (winner: string) =
         match moves.Split " " |> play Game.NewGame with
         | Won (_, _, o, _) | Drawn (_, _, o, _) ->
-            o.WhoWon ()
-            |> Option.map Player.toString
-            |> Option.defaultValue "_"
-            |> should equal winner
+            o.WhoWon () |> should bePlayer winner
         | _ -> fail ()
 
     [<Theory>]
@@ -93,9 +65,7 @@ module Tests =
             g.Positions
             |> Seq.find (Positions.toString >> ((=) position))
             |> g.PlayerAt
-            |> Option.map Player.toString
-            |> Option.defaultValue "_"
-            |> should equal player
+            |> should bePlayer player
 
     [<Theory>]
     [<InlineData ("NW C SE N S SW NE W E", false)>]

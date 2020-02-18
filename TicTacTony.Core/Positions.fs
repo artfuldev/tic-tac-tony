eb956b46d5704af21343929ff83366c66305c1c4
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2020-02-18 08:23:05 +0530

    Improve readability

--- TicTacTony.Core/Positions.fs
+++ TicTacTony.Core/Positions.fs
@@ -2,33 +2,23 @@
 
 type Position =
   internal
-  | NW
-  | N
-  | NE
-  | W
-  | C
-  | E
-  | SW
-  | S
-  | SE
+  | NW |  N | NE
+  |  W |  C |  E
+  | SW |  S | SE
 
 module Positions =
 
   let internal all =
-    [ NW; N; NE;
-      W; C; E;
-      SW; S; SE ]
+    seq [
+      NW;  N; NE;
+       W;  C;  E;
+      SW;  S; SE
+    ]
 
   let toString = function
-    | NW -> "NW"
-    | N -> "N"
-    | NE -> "NE"
-    | W -> "W"
-    | C -> "C"
-    | E -> "E"
-    | SW -> "SW"
-    | S -> "S"
-    | SE -> "SE"
+    | NW -> "NW" | N -> "N" | NE -> "NE"
+    |  W ->  "W" | C -> "C" |  E ->  "E"
+    | SW -> "SW" | S -> "S" | SE -> "SE"
 
   let parse value =
-    all |> List.tryFind (toString >> ((=) value))
+    all |> Seq.tryFind (toString >> ((=) value))

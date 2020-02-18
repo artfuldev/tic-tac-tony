170ae90a87f7f644e09a38d9b67d0423b58f9733
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2020-02-18 19:13:38 +0530

    Update to use new primitives API

--- TicTacTony.Console/Parser.fs
+++ TicTacTony.Console/Parser.fs
@@ -1,7 +1,8 @@
 ﻿namespace TicTacTony.Console
 
-open TicTacTony.Core.Positions
+open TicTacTony.Core.Board
 open System.Text.RegularExpressions
+open Option
 
 
 module Parser =
@@ -13,11 +14,11 @@ module Parser =
             then [ for g in result.Groups -> g.Value ] |> List.tail |> Some
             else None
 
+    let private _pos value = positions |> Seq.tryFind (string >> ((=) value))
+
     let parse = function
-        | Regex "^M (NW|N|NE|W|C|E|SW|S|SE)$" [position] ->
-            position |> parse |> Option.map Move
-        | Regex "^P (NW|N|NE|W|C|E|SW|S|SE)$" [position] ->
-            position |> parse |> Option.map PlayerAt
+        | Regex "^M (NW|N|NE|W|C|E|SW|S|SE)$" [x] -> x |> _pos |> map Move
+        | Regex "^P (NW|N|NE|W|C|E|SW|S|SE)$" [x] -> x |> _pos |> map PlayerAt
         | Regex "^I$" _ -> Some IsDraw
         | Regex "^W$" _ -> Some WhoWon
         | Regex "^T$" _ -> Some TakeBack

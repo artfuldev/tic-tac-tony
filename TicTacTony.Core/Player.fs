68788f04f2a75bbedc0262179ca03d5bb6ee43cb
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2020-02-18 19:11:24 +0530

    Simplify primitives

--- TicTacTony.Core/Player.fs
+++
@@ -1,8 +0,0 @@
-﻿namespace TicTacTony.Core
-
-
-type Player = internal X | O
-
-module Player =
-
-    let toString = function X -> "X" | O -> "O"
--- TicTacTony.Core/Primitives.fs
+++ TicTacTony.Core/Primitives.fs
@@ -1,9 +1,15 @@
 ﻿namespace TicTacTony.Core
 
 
+type Position =
+    | NW |  N | NE
+    |  W |  C |  E
+    | SW |  S | SE
+
+type Player = X | O
+
 type Move = private Move of Position * Player
 
 module Move =
     
     let position = function Move (position, _) -> position
-

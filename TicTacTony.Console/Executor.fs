c7aac1c60d96f21ed48c3304107a3a935757f5a3
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2020-02-18 21:38:09 +0530

    Upgrade to latest core API while simplifying

--- TicTacTony.Console/Executor.fs
+++ TicTacTony.Console/Executor.fs
@@ -6,75 +6,56 @@ open TicTacTony.Console
 open Reader
 open System
 
+open Move
+open Helpers
+open Game
 
 module Executor =
-  
-    open Move
 
     let private moves p =
         p.Moves |> Seq.map (position >> Move >> Some)
   
-    let private options f o u p =
-        seq [
-            defaultValue Seq.empty (map moves p)
-            Board.positions |> Seq.map (PlayerAt >> Some)
-            seq [
-                (map (fun (f: IFull) -> IsDraw) f)
-                (map (fun (o: IOver) -> WhoWon) o)
-                (map (fun (u: IUndoable) -> TakeBack) u)
-                Some New; Some Exit
-            ]
+    let private options game =
+        [ ifPlayable (k Seq.empty) moves
+        ; Board.positions |> Seq.map (PlayerAt >> Some) |> k
+        ; ifFull (k Seq.empty) (k (Seq.singleton (Some IsDraw)))
+        ; ifOver (k Seq.empty) (k (Seq.singleton (Some WhoWon)))
+        ; ifUndoable (k Seq.empty) (k (Seq.singleton (Some TakeBack)))
+        ; seq [Some New; Some Exit] |> k
         ]
+        |> Seq.map (apply game)
         |> Seq.concat
         |> Seq.choose id
 
-    let private playerAt p g =
-        let sprint = sprintf "The cell at %s is %s" (string p)
-        let suffix =
-            match g.PlayerAt p with
-            | Some x -> sprintf "filled by %s" (string x)
-            | None -> "empty"
-        in sprint suffix
+    let private player p' = p' |> map string |> defaultValue "Nobody"
 
-    let private whoWon o =
-        match o.WhoWon() with
-        | Some x -> sprintf "The winner is %s." (string x)
-        | None -> "Nobody won."
+    let private playerAt p g = g.PlayerAt p |> player
 
-    let private isDraw f =
-        if f.IsDraw () then "It's a draw." else "It's not a draw."
+    let private whoWon o = o.WhoWon() |> player
 
-    let private takeBack u =
-        u.TakeBack ()
+    let private isDraw f = if f.IsDraw () then "Yes" else "No"
 
-    let private move x p =
-        p.Moves |> Seq.find (position >> ((=) x)) |> p.Move
+    let private takeBack u = u.TakeBack ()
 
-    let rec private step game g f o u p =
-        let _ = Board.toString g.Board |> printfn "\n%s"
-        let command = read (options f o u p)
-        in handle game g f o u p command
+    let private move x p = p.Moves |> Seq.find (position >> ((=) x)) |> p.Move
 
-    and private handle game g f o u p command =
-        let print =
-            printfn "%s\nPress any key to proceed..."
-            >> Console.ReadKey >> ignore
-        let current f x = let _ = iter (f >> print) x in game
-        let next f x = map f x |> defaultValue game
-        let _ = printfn "%s" (Commands.toDescription command)
-        in
+    let private handle continuation game command =
+        let print = printfn "%s" >> Console.ReadKey >> k game
+        let fail _ = failwith "impossible"
+        let play =
             match command with
-            | New -> Game.NewGame |> play
-            | Move x -> p |> next (move x) |> play
-            | PlayerAt x -> Some g |> current (playerAt x) |> play
-            | IsDraw -> f |> current isDraw |> play
-            | WhoWon -> o |> current whoWon |> play
-            | TakeBack -> u |> next takeBack |> play
-            | Exit -> exit 0
+            | Exit -> fun _ -> exit 0
+            | New -> k NewGame
+            | Move x -> ifPlayable fail (move x)
+            | PlayerAt x -> onGame (playerAt x >> print)
+            | IsDraw -> ifFull fail (isDraw >> print)
+            | WhoWon -> ifOver fail (whoWon >> print)
+            | TakeBack -> ifUndoable fail takeBack
+        let game = Commands.toDescription command |> printfn "%s" |> k game
+        in game |> play |> continuation
 
-    and play game =
-        match game with
-        | Fresh (g, p) -> step game g None None None (Some p)
-        | Played (g, u, p) -> step game g None None (Some u) (Some p)
-        | Won (g, f', o, u) -> step game g f' (Some o) (Some u) None
-        | Drawn (g, f, o, u) -> step game g (Some f) (Some o) (Some u) None
+    let rec play game =
+        let _ = onGame (fun g -> Board.toString g.Board |> printfn "\n%s") game
+        in s (handle play) (options >> read) game
+
+    

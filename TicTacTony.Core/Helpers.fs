2a895a70da802b84e340e5287e1f229ff8d87609
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2020-02-18 21:36:52 +0530

    Add apply

--- TicTacTony.Core/Helpers.fs
+++ TicTacTony.Core/Helpers.fs
@@ -1,10 +1,12 @@
 ﻿namespace TicTacTony.Core
 
 
-module internal Helpers =
+module Helpers =
 
     let flip f x y = f y x
     
     let k x _ = x
 
     let s x y z = x z (y z)
+
+    let apply x f = f x

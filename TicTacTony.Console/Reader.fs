191be1d2122ed00fbd090e6ad09d2654590405b2
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2020-02-19 02:47:07 +0530

    Simplify reading commands

--- TicTacTony.Console/Reader.fs
+++ TicTacTony.Console/Reader.fs
@@ -1,27 +1,26 @@
 ﻿namespace TicTacTony.Console
 
-open Commands
 open Parser
 open System
+open TicTacTony.Core.Board
 
 
 module Reader =
-    let read options =
-        let _ = printfn "Choose from:"
-        let _ = options |> Seq.map toString |> Seq.iter (printfn "%s")
-        let rec _read allowed =
-            let _ =
-                printfn "%s %s"
-                    "Enter your choice here, without []"
-                    "eg: for Player at NW, type P NW"
-            let retry () =
-                let _ = printfn "invalid command, try again" in _read allowed
-            in
-                match Console.ReadLine () |> parse with
-                | None -> retry ()
-                | Some command ->
-                    if allowed |> Seq.contains command
-                    then command
-                    else retry ()
-        in _read options
+
+    let read game =
+        let parse = parse game
+        let _ =
+            [ positions |> Seq.map (string >> sprintf "M %s")
+            ; positions |> Seq.map (string >> sprintf "P %s")
+            ; seq ["I"; "W"; "T"; "N"; "E"]
+            ] |> Seq.concat |> Seq.map parse |> Seq.choose id
+            |> Seq.map Commands.toString
+            |> Seq.append (Seq.singleton "Choose from:")
+            |> Seq.iter (printfn "%s")
+        let rec read parse =
+            let _ = printfn "%s" "Enter your choice here, without []:" in
+            match Console.ReadLine () |> parse with
+            | None -> let _ = printfn "invalid, try again" in read parse
+            | Some command -> command
+        in read parse
 

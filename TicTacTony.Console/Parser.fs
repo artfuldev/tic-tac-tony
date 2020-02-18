4db7496dd2279889a833a0910a3a514db716fac0
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2020-02-19 02:46:58 +0530

    Upgrade command parsing for new API

--- TicTacTony.Console/Parser.fs
+++ TicTacTony.Console/Parser.fs
@@ -1,8 +1,11 @@
 ﻿namespace TicTacTony.Console
 
-open TicTacTony.Core.Board
+open TicTacTony.Core
 open System.Text.RegularExpressions
 open Option
+open TicTacTony.Core.Board
+open TicTacTony.Console
+open Move
 
 
 module Parser =
@@ -14,14 +17,34 @@ module Parser =
             then [ for g in result.Groups -> g.Value ] |> List.tail |> Some
             else None
 
-    let private _pos value = positions |> Seq.tryFind (string >> ((=) value))
-
-    let parse = function
-        | Regex "^M (NW|N|NE|W|C|E|SW|S|SE)$" [x] -> x |> _pos |> map Move
-        | Regex "^P (NW|N|NE|W|C|E|SW|S|SE)$" [x] -> x |> _pos |> map PlayerAt
-        | Regex "^I$" _ -> Some IsDraw
-        | Regex "^W$" _ -> Some WhoWon
-        | Regex "^T$" _ -> Some TakeBack
-        | Regex "^E$" _ -> Some Exit
+    let parse game = function
+        | Regex "^M (NW|N|NE|W|C|E|SW|S|SE)$" [x] ->
+            match game with
+            | Fresh (_, p) | Played (_, _, p) ->
+                p.Moves
+                |> Seq.tryFind (position >> string >> ((=) x))
+                |> map (fun x -> Play (x, p))
+            | _ -> None
+        | Regex "^P (NW|N|NE|W|C|E|SW|S|SE)$" [x] ->
+            match game with
+            | Fresh (g, _) | Played (g, _, _) | Won (g, _, _, _)
+            | Drawn (g, _, _, _) ->
+                positions
+                |> Seq.tryFind (string >> ((=) x))
+                |> map (fun x -> PlayerAt (x, g))
+        | Regex "^I$" _ ->
+            match game with
+            | Won (_, Some f, _, _) | Drawn (_, f, _, _) -> IsDraw f |> Some
+            | _ -> None
+        | Regex "^W$" _ ->
+            match game with
+            | Won (_, _, o, _) | Drawn (_, _, o, _) -> WhoWon o |> Some
+            | _ -> None
+        | Regex "^T$" _ ->
+            match game with
+            | Played (_, u, _) | Won (_, _, _, u) | Drawn (_, _, _, u) ->
+                TakeBack u |> Some
+            | _ -> None
         | Regex "^N$" _ -> Some New
+        | Regex "^E$" _ -> Some Exit
         | _ -> None

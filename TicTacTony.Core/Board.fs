d32f969a0215327debef01d89c720b12663c2e01
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2020-02-25 10:46:39 +0530

    Make entire module internal

--- TicTacTony.Core/Board.fs
+++ TicTacTony.Core/Board.fs
@@ -7,14 +7,14 @@ open Option
 
 type Board = internal Board of Move list
 
-module Board =
+module internal Board =
 
-    let rec internal playerAt x = function
+    let rec playerAt x = function
         | Board (Move (y, p)::_) when x = y -> Some p
         | Board (_::ms) -> playerAt x (Board ms)
         | _ -> None
 
-    let internal winner board =
+    let winner board =
         let winner = function
             | [ Some a; Some b; Some c ] ->
                 if (a = b && b = c) then Some a else None
@@ -28,26 +28,26 @@ module Board =
             |> Seq.choose id
             |> Seq.tryHead
     
-    let internal player = function
+    let player = function
         | Board ms -> if List.length ms % 2 <> 1 then X else O
 
-    let internal isFull = function
+    let isFull = function
         | Board ms -> List.length ms = 9
 
-    let internal isWon =
+    let isWon =
         winner >> isSome
     
     let positions = seq [ NW;  N; NE;  W;  C;  E; SW;  S; SE ]
 
-    let internal unoccupied = function
+    let unoccupied = function
         | Board ms ->
             let occupied = flip Seq.contains (ms |> List.map Move.position)
             in positions |> Seq.filter (not << occupied)
 
-    let internal make m = function
+    let make m = function
         | Board ms -> Board (m::ms)
 
-    let internal undo = function
+    let undo = function
         | Board [] -> failwith "impossible" | Board (_::ms) -> Board ms
 
     let toString board =

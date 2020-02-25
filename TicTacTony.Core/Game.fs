99d37e407fb5857c573de5be5849c8c121379455
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2020-02-26 00:20:29 +0530

    Improve API

--- TicTacTony.Core/Game.fs
+++ TicTacTony.Core/Game.fs
@@ -9,7 +9,7 @@ type IGame = private | New | Played of Position * IGame
 
 and IUndoable = private | Previous of IGame
 
-type IPlayable = private | Moves of Move list
+type IPlayable = private | Playable of Player * Move list
 
 and Move = private | Move of Position * (unit -> Game)
 
@@ -22,15 +22,17 @@ module Game =
 
     open Board
     open Helpers
+    
+    let other = function | X -> O | O -> X
 
-    let rec private board game =
-        let other = function | X -> O | O -> X
-        let rec xOrO = function | New -> X | Played (_, g) -> g |> xOrO |> other
-        in
-            match game with
-            | New -> Map.empty | Played (x, g) -> make x (xOrO g) (board g)
+    let rec private xOrO = function
+        | New -> X | Played (_, g) -> g |> xOrO |> other
+
+    let rec private board = function
+        | New -> Map.empty | Played (x, g) -> make x (xOrO g) (board g)
 
     let rec private create game =
+        let p = game |> xOrO
         let full =
             let full = if game |> board |> isWon then NoDraw else Draw
             in if game |> board |> Map.count |> (=) 9 then Some full else None
@@ -42,14 +44,14 @@ module Game =
             let moves game =
                 let move pos = Move (pos, Played (pos, game) |> create |> k)
                 in game |> board |> unoccupied |> List.map move
-            if over <> None then None else Moves (game |> moves) |> Some
+            if over <> None then None else Playable (p, game |> moves) |> Some
         let undoable =
             match game with | New -> None | Played (_, g) -> Previous g |> Some
         in Game (game, playable, undoable, over, full)
 
     let NewGame = create New
 
-    let moves (Moves moves) = moves
+    let moves (Playable (_, moves)) = moves
 
     let make = function Move (_, f) -> f ()
 
@@ -60,6 +62,8 @@ module Game =
     let isDraw = function | Draw -> true | NoDraw -> false
 
     let playerAt = function Game (g, _, _, _, _) -> g |> board |> flip playerAt
+
+    let player (Playable (player, _)) = player
 
     let toString = function Game (g, _, _, _, _) -> g |> board |> toString
 

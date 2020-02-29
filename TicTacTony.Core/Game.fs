0e93bbb3d4ee019ec4936430dd05217ae60f54a5
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2020-02-29 09:08:01 +0530

    Rewrite the Game API

--- TicTacTony.Core/Game.fs
+++ TicTacTony.Core/Game.fs
@@ -1,72 +1,137 @@
 ﻿namespace TicTacTony.Core
 
+open Board
+open Player
 
-type IFull = private | Draw | NoDraw
+type Full = private | Draw | NoDraw
 
-type IOver = private | ByWin of Player | ByDraw
+type Over = private | ByWin of Player | ByDraw
 
-type IGame = private | New | Played of Position * IGame
+type State = private | New | Played of Position * State
 
-and IUndoable = private | Previous of IGame
+and Playable = private | Playable of Move list
 
-type IPlayable = private | Playable of Player * Move list
+and Move = private | Move of Position * (unit -> IUndoable)
 
-and Move = private | Move of Position * (unit -> Game)
+and Undoable = private | Previous of IPlayable
 
-and Game =
-    | Game of
-        IGame *
-        IPlayable option * IUndoable option * IOver option * IFull option
+and IGame = abstract member State: State
+
+and IPlayable = inherit IGame abstract member Playable: Playable
+
+and IUndoable = inherit IGame abstract member Undoable: Undoable
+
+and IOver = inherit IUndoable abstract member Over: Over
+
+and IFull = inherit IOver abstract member Full: Full
+
+type internal IPlaying = inherit IPlayable inherit IUndoable
+
+type internal IWon = inherit IOver
+
+type internal IWinnable = inherit IPlaying
+
+module Move =
+    
+    let position = function Move (x, _) -> x
+
+module internal Internal =
+
+    let rec player = function
+        | New -> X | Played (_, g) -> g |> player |> other
+
+    let rec board = function
+        | New -> Map.empty | Played (x, g) -> make x (player g) (board g)
+    
+    let moves (Playable moves) = moves
+    
+    let takeBack (Previous previous) = previous
+    
+    let whoWon = function | ByWin x -> Some x | ByDraw -> None
+    
+    let isDraw = function | Draw -> true | NoDraw -> false
+
+    type Game (state: State) = interface IGame with member _.State = state
+     
+    type PlayedGame (position: Position, previous: IPlayable) =
+        inherit Game (Played (position, previous.State))
+        interface IUndoable with member _.Undoable = Previous previous
+     
+    type WonGame (position: Position, previous: IWinnable) =
+        inherit PlayedGame (position, previous)
+        interface IWon with member _.Over = ByWin (player previous.State)
+     
+    type DrawnGame (position: Position, previous: IWinnable) =
+        inherit PlayedGame (position, previous)
+        interface IOver with member _.Over = ByDraw
+        interface IFull with member _.Full = Draw
+     
+    type WonOnLastMoveGame (position: Position, previous: IWinnable) =
+        inherit WonGame (position, previous)
+        interface IFull with member _.Full = NoDraw
+     
+    type InProgressGame  (position: Position, previous: IPlayable) =
+        inherit PlayedGame (position, previous)
+        abstract member _Move: Position -> (unit -> IUndoable)
+        interface IPlaying with
+            member game.Playable =
+                let move position = Move (position, position |> game._Move)
+                let free = (game :> IGame).State |> board |> free
+                in Position.all |> List.where free |> List.map move |> Playable
+        default game._Move position = fun () ->
+            let occupied = not << ((game :> IGame).State |> board |> free)
+            let played = Position.all |> List.where occupied |> List.length
+            in
+                if played >= 3
+                then upcast WinnableGame (position, game)
+                else upcast InProgressGame (position, game)
+     
+    and WinnableGame (position: Position, previous: IPlayable) =
+        inherit InProgressGame (position, previous)
+        interface IWinnable
+        override game._Move position = fun () ->
+            let board = (game :> IGame).State |> board
+            let occupied = not << free board
+            let played = Position.all |> List.where occupied |> List.length
+            let player = (game :> IGame).State |> player
+            let board' = make position player board
+            let win = isWon board'
+            in
+                if played = 8 then
+                    if win
+                    then upcast WonOnLastMoveGame (position, game)
+                    else upcast DrawnGame (position, game)
+                else
+                    if win
+                    then upcast WonGame (position, game)
+                    else upcast WinnableGame (position, game)
+     
+    type NewGame () =
+        inherit Game (New)
+        interface IPlayable with
+            member g.Playable =
+                let move x = Move (x, fun _ -> upcast InProgressGame (x, g))
+                in Position.all |> List.map move |> Playable
 
 module Game =
 
-    open Board
     open Helpers
+    open Internal
+
+    let NewGame = NewGame () :> IGame
+
+    let moves (game: IPlayable) = moves game.Playable
     
-    let other = function | X -> O | O -> X
-
-    let rec private xOrO = function
-        | New -> X | Played (_, g) -> g |> xOrO |> other
-
-    let rec private board = function
-        | New -> Map.empty | Played (x, g) -> make x (xOrO g) (board g)
-
-    let rec private create game =
-        let p = game |> xOrO
-        let full =
-            let full = if game |> board |> isWon then NoDraw else Draw
-            in if game |> board |> Map.count |> (=) 9 then Some full else None
-        let over =
-            let b = game |> board
-            let over = match b |> winner with | Some x -> ByWin x | _ -> ByDraw
-            in if b |> isWon || full <> None then Some over else None
-        let playable =
-            let moves game =
-                let move pos = Move (pos, Played (pos, game) |> create |> k)
-                in game |> board |> unoccupied |> List.map move
-            if over <> None then None else Playable (p, game |> moves) |> Some
-        let undoable =
-            match game with | New -> None | Played (_, g) -> Previous g |> Some
-        in Game (game, playable, undoable, over, full)
-
-    let NewGame = create New
-
-    let moves (Playable (_, moves)) = moves
-
     let make = function Move (_, f) -> f ()
 
-    let takeBack (Previous previous) = create previous
+    let takeBack (game: IUndoable) = takeBack game.Undoable
 
-    let whoWon = function | ByWin x -> Some x | ByDraw -> None
+    let whoWon (game: IOver) = whoWon game.Over
 
-    let isDraw = function | Draw -> true | NoDraw -> false
+    let isDraw (game: IFull) = isDraw game.Full
 
-    let playerAt = function Game (g, _, _, _, _) -> g |> board |> flip playerAt
+    let playerAt (game: IGame) = game.State |> board |> flip playerAt
 
-    let player (Playable (player, _)) = player
+    let player (game: IPlayable) = player game.State
 
-    let toString = function Game (g, _, _, _, _) -> g |> board |> toString
-
-    let positions = positions
-    
-    let position = function Move (x, _) -> x
+    let toString (game: IGame) = game.State |> board |> toString

da9fdbc9edc40bcd803577c3447d8023f1bdeefd
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2020-02-29 09:06:49 +0530

    Add primitive modules

--- TicTacTony.Core/Primitives.fs
+++ TicTacTony.Core/Primitives.fs
@@ -6,4 +6,13 @@ type Position =
     |  W |  C |  E
     | SW |  S | SE
 
+module Position =
+    
+    let all = [ NW;  N; NE;  W;  C;  E; SW;  S; SE ]
+
 type Player = X | O
+
+module Player =
+    
+    let other = function | X -> O | O -> X
+    
